1242a315ac2be6762ea5bd35c4ace780f84ca6b8
root <root@debian>
2015-12-08 23:37:39 +0100

    mongodb instalado

--- script.sh
+++ script.sh
@@ -7,4 +7,6 @@ wget http://nodejs.org/dist/v0.10.32/node-v0.10.32-linux-x86.tar.gz
 tar xvf node-v0.10.32-linux-x86.tar.gz
 cd node-v0.10.32-linux-x86/
 cp * /usr/local/ -r
+npm install -g express
 npm install -g express-generator
+npm install mongodb
